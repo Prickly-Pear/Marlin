e5185dddf88a42777ef6642b97e02b84a7239d14
Prickly-Pear <62167540+Prickly-Pear@users.noreply.github.com>
2020-12-31 14:06:11 +1100

    xxx

--- config/Configuration_adv.h
+++ config/Configuration_adv.h
@@ -1927,11 +1927,11 @@
 // The number of linear moves that can be in the planner at once.
 // The value of BLOCK_BUFFER_SIZE must be a power of 2 (e.g. 8, 16, 32)
 #if BOTH(SDSUPPORT, DIRECT_STEPPING)
-  #define BLOCK_BUFFER_SIZE  2
+  #define BLOCK_BUFFER_SIZE  8
 #elif ENABLED(SDSUPPORT)
-  #define BLOCK_BUFFER_SIZE 2
+  #define BLOCK_BUFFER_SIZE 16
 #else
-  #define BLOCK_BUFFER_SIZE 2
+  #define BLOCK_BUFFER_SIZE 16
 #endif
 
 // @section serial
